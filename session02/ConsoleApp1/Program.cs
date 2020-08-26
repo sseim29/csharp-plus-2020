cf041bbd1f23b6781b034dd9bec0820a33c7806f
Seim G <Seim_ghede@hotmail.com>
2020-08-26 11:55:20 +0200

    Namn Program

---
+++ session02/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Vill du skriva i ditt namn? (y/n)?");
+            var key = Console.ReadKey();
+
+            if (key.KeyChar == 'n') return;
+
+            Console.WriteLine("Skriv in ditt namn ");
+            var name = Console.ReadLine();
+
+            Console.WriteLine(" Hej på dig " + name);
+            Console.ReadKey();
+        }
+    }
+}
--- session02/Session02exercise01/Program.cs
+++ session02/Session02exercise01/Program.cs
@@ -7,12 +7,11 @@ namespace Session02exercise01
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-
+            
             var integer = 0;
-
             Console.WriteLine("Integer is " + integer.ToString());
+            
             string stringvalue = "MyStringValue";
-
             Console.WriteLine("The value of stringvalue is: " + stringvalue);
 
 
